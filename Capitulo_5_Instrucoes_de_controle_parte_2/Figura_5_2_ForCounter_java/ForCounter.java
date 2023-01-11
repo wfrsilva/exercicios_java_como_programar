e2bf7d506ffec14eae41132a94d0ffab3b309bfb
Wendel (CIGA) Silva <wfrsilva@gmail.com>
2023-01-11 20:52:46 -0300

    Figura 5.2: ForCounter.java

---
+++ Capitulo_5_Instrucoes_de_controle_parte_2/Figura_5_2_ForCounter_java/ForCounter.java
@@ -0,0 +1,19 @@
+/* D:\Google Drive\UFSC_2017-01\Java como programar 10 edição
+pagina 121
+// Figura 5.2: ForCounter.java
+2 // Repetição controlada por contador com a instrução de repetição for. */
+package Capitulo_5_Instrucoes_de_controle_parte_2.Figura_5_2_ForCounter_java;
+
+public class ForCounter{
+
+    public static void main(String [] args ){
+        
+        for (int counter = 1; counter <=10; counter++){
+            System.out.printf("%d ", counter);
+        }//for
+
+        System.out.println();
+
+    }//main
+
+}//ForCounter
