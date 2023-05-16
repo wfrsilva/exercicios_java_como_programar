c9ee06beff3e50413a70e75802aff6cf51ee8451
Wendel (CIGA) Silva <wfrsilva@gmail.com>
2023-05-15 23:53:17 -0300

    Figura 5.14: ContinueTest.java
    // Figura 5.14: ContinueTest.java
    // Instrução continue que termina uma iteração de uma instrução for.

---
+++ Capitulo_5_Instrucoes_de_controle_parte_2/Figura_5_14_ContinueTest_java/ContinueTest.java
@@ -0,0 +1,20 @@
+package Capitulo_5_Instrucoes_de_controle_parte_2.Figura_5_14_ContinueTest_java;
+// Figura 5.14: ContinueTest.java
+// Instrução continue que termina uma iteração de uma instrução for.
+
+public class ContinueTest{
+    public static void main (String [] args){
+        for (int count = 1; count <=10; count++){
+            if(count == 5){
+                continue;
+            }//if
+
+            System.out.printf("%d ", count);
+
+        } //for
+
+        System.out.printf("%nUsed continue to skip printing 5%n");
+
+    }//main
+
+}//ContinueTest
