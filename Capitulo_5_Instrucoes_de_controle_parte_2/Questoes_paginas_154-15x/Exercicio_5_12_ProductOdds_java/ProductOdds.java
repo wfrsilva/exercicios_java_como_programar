3ebe9a28370cde0f3b270330634e68db17327525
Wendel (CIGA) Silva <wfrsilva@gmail.com>
2023-08-10 20:04:38 -0300

    Create ProductOdds.java
    pagina 154
    //5.12 (Calculando o produto de números inteiros ímpares)
    Escreva um aplicativo que calcula o produto dos números inteiros ímpares de 1 a 15.
    5.12 (Calculating the product of odd integers)
    Write an application that calculates the product of odd integers from 1 to 15.*/

---
+++ Capitulo_5_Instrucoes_de_controle_parte_2/Questoes_paginas_154-15x/Exercicio_5_12_ProductOdds_java/ProductOdds.java
@@ -0,0 +1,26 @@
+
+/* D:\Google Drive\UFSC_2017-01\Java como programar 10 edição
+pagina 154 
+//5.12 (Calculando o produto de números inteiros ímpares) 
+Escreva um aplicativo que calcula o produto dos números inteiros ímpares de 1 a 15.
+5.12 (Calculating the product of odd integers)
+Write an application that calculates the product of odd integers from 1 to 15.*/
+package Exercicio_5_12_ProductOdds_java;
+
+public class ProductOdds {
+
+    public static void main(String[] args) {
+        int result = 0;
+
+        for (int i = 0; i <= 15; i++) {
+            if (i % 2 != 0) {
+                System.out.printf("%d + %d = %d%n", i, result, (result + i));
+                result = result + i;
+            } // if
+        } // for
+
+        System.out.println("\nA soma dos impares de 1 a 15 é " + result);
+
+    }// main
+
+}// ProductOdds
