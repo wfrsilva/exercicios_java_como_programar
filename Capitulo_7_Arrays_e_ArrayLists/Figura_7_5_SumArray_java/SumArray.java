e79d6013f054008ec760c8e0d2c189eb3f78ba73
Wendel (CIGA) Silva <wfrsilva@gmail.com>
2023-09-13 21:03:05 -0300

    Total of array elements: 849
    Total of array elements: 849

--- Capitulo_7_Arrays_e_ArrayLists/Figura_7_5_SumArray_java/SumArray.java
+++ Capitulo_7_Arrays_e_ArrayLists/Figura_7_5_SumArray_java/SumArray.java
@@ -8,7 +8,7 @@ public class SumArray {
 
     public static void main(String[] args) {
 
-        int[] array = { 87, 68, 94, 100, 83, 7, 85, 91, 76, 87 };
+        int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
         int total = 0;
 
         for (int counter = 0; counter < array.length; counter++) {
